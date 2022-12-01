bd23a0e5b596c36990f5dfef88757cd4bcdfe5a2
niknug <niknug@yandex.ru>
2022-12-02 01:26:15 +0300

    Add detail fragments

--- app/src/main/java/ru/niknug/android/mynotes/Author.kt
+++ app/src/main/java/ru/niknug/android/mynotes/Author.kt
@@ -2,4 +2,4 @@ package ru.niknug.android.mynotes
 
 import java.util.*
 
-data class Author(val id: UUID, val name: String, val birthdate: Date)
+data class Author(val id: UUID, val name: String, val dateOfBirth: Date, val dateOfDeath: Date?)
